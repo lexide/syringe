0a6ee89a099738f99037ca74312102df06047cb9
Danny Smart <danny.smart@lexisnexisrisk.com>
2018-02-20 10:39:27 +0000

    Add new test for named tag keys and refactor PrivacyTest to use assertions

---
+++ tests/integration/TagTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lexide\Syringe\IntegrationTests;
+
+
+use Lexide\Syringe\IntegrationTests\Service\CollectionService;
+use Lexide\Syringe\Syringe;
+
+class TagTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Pimple\Container
+     */
+    protected $container;
+
+    public function setUp()
+    {
+        $configFiles = [
+            "service.json",
+            "private_test" => "aliased.json"
+        ];
+
+        Syringe::init(__DIR__, $configFiles);
+        $this->container = Syringe::createContainer();
+    }
+
+    public function testTags()
+    {
+        /** @var CollectionService $collection */
+        $collection = $this->container["tagCollection"];
+
+        $this->assertNotEmpty($collection->services, "No services were injected using the tag #duds");
+        $this->assertCount(2, $collection->services, "Tag collection contained an unexpected number of services (" . count($collection->services) . ")");
+
+        $tagNames = array_flip(array_keys($collection->services));
+
+        $this->assertArrayHasKey("testKey", $tagNames, "Name was missing from the tagged service");
+
+        unset($tagNames["testKey"]);
+        $unnamedKey = array_pop($tagNames);
+
+        $this->assertNotFalse($unnamedKey, "Unnamed service was not injected");
+
+        $this->assertInstanceOf(
+            "\\Lexide\\Syringe\\IntegrationTests\\Service\\DudService",
+            $collection->services[$unnamedKey],
+            "An incorrect service was injected: " . print_r($collection->services, true)
+        );
+    }
+}
